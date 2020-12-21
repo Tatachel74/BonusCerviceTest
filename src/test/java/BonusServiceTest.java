c9f27e2b656a6cff2718f6147cc2452a1fbd7938
Tanya <tanya.butova@gmail.com>
2020-12-21 19:20:14 +0500

    BonusServisTest

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -7,22 +7,54 @@ class BonusServiceTest {
     @Test
     void shouldCalculateForRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
+
         long amount = 1000_60;
         boolean registered = true;
         long expected = 30;
+
         long actual = service.calculate(amount, registered);
         boolean passed = expected == actual;
+
         assertEquals(expected, actual);
     }
-
     @Test
     void shouldCalculateNotRegisteredAndOverLimit() {
         BonusService service = new BonusService();
+
         long amount = 1000_60;
         boolean registered = false;
         long expected = 10;
+
         long actual = service.calculate(amount, registered);
         boolean passed = expected == actual;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldCalculateForNotRegisteredAndIsNotLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 2_100_000;
+        boolean registered = false;
+        long expected = 210;
+
+        long actual = service.calculate(amount, registered);
+        boolean passed = expected == actual;
+
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void shouldCalculateForRegisteredAndIsNotLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 2_100_000;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+        boolean passed = expected == actual;
+
         assertEquals(expected, actual);
     }
 }
